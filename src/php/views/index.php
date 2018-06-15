62983982322266747c8b1f9a51050cb8cbe3e2d6
Lemmon <lemmon@users.noreply.github.com>
2018-06-15 12:56:35 +0200

    web app

--- src/php/views/index.php
+++ src/php/views/index.php
@@ -2,6 +2,10 @@
 <html>
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="white">
+  <meta name="apple-mobile-web-app-title" content="Todo">
   <title>todo</title>
   <link rel="stylesheet" href="app.css">
   <link rel="stylesheet" href="https://unpkg.com/abrusco@0.6.3/css/abrusco.min.css">
